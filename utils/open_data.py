084535f5a6d0f8ea78adf2a9ebb63b8443083159
Nathalie Rombeek <nathalie.rombeek@meteoswiss.ch>
2023-01-05 12:31:11 +0100

    add utils

---
+++ utils/open_data.py
@@ -0,0 +1,201 @@
+"""
+Methods for opening and processing data
+"""
+
+import glob
+import os
+import numpy as np
+from netCDF4 import Dataset
+import json
+
+from utils.transformation import coordx2ind, coordy2ind
+
+def get_meta(dir):
+    """
+    Load metadata from a JSON file.
+
+    Args:
+    -----
+    dir: str
+     The directory containing the JSON file.
+    
+    Returns:
+    --------
+    meta: dict
+     A dictionary containing the metadata from the JSON file
+    """
+
+    with open(os.path.join(dir, "totalroi.json")) as f:
+        metajson = f.readlines()
+    meta = json.loads(metajson[0])
+    f.close()
+    return meta
+
+def get_precip_files(dir,prd="RZC"):
+    """
+    Get a list of precipitation files in a given directory. The product can be
+     either "RZC" or "CPC". The file format is determined based on the metadata
+     for the directory.
+
+    Args:
+    -----
+    dir: str
+     The main directory containing the precipitation files.
+    prd: str
+     The product for which to get the files. Can be either "RZC" (default)
+     or "CPC".
+    
+    Returns:
+    --------
+    allFiles: list
+     A list of filenames for the precipitation files with a 5-minute timestep, 
+     for the specified product
+    """
+    meta = get_meta(dir)
+    if meta["fmt"] == ".gif":
+        allFiles = glob.glob(os.path.join(dir, prd, "*.gif"))
+    elif meta["fmt"] == ".h5":
+        allFiles = glob.glob(os.path.join(dir, prd, "*.h5"))
+    return allFiles
+
+
+def get_totalDomain(inDir,prd):
+    """
+    Get the total precipitation for a region over time.    
+    
+    Args:
+    -----
+    inDir: str
+     The directory containing the binary file.
+    prd: str
+     The product for which the data is intended, either "CPC" or "RZC"
+
+    Returns:
+    --------
+    totalDomain: ndarray
+     A 3D array containing the total precipitation data, 
+     with dimensions (time, y, x).
+    """
+    meta = get_meta(inDir)
+    dtype = np.dtype('f')
+    if (meta['prd'] != prd):
+        print("Be careful, you are mixing up products!")
+    f = open(os.path.join(inDir,"totalroi.bin"), "rb")
+    rdata = np.fromfile(f,dtype)
+    totalDomain = np.flipud(np.rot90(np.transpose(rdata.reshape([int(meta['z']), int(meta['height']), int(meta['width'])]))))
+    totalDomain[totalDomain < 0] = -1.0
+    return totalDomain
+
+def get_totalRoi(rectangle,totalDomain):
+    """
+    Get a subregion of an array representing a region of interest.
+
+    Args:
+    -----
+    rectangle: tuple
+     A tuple specifying the bounds of the region as (xmin, xmax, ymin, ymax).
+    totalDomain: ndarray
+     Array cotaining the whole region region.
+
+    Returns:
+    --------
+    totalRoi: ndarray
+     An array containing precipitation within the specified region.
+    """
+    totalRoi = totalDomain[coordy2ind(rectangle[3]):coordy2ind(rectangle[2]), coordx2ind(rectangle[0]):coordx2ind(rectangle[1]),:]
+    totalRoi[totalRoi < 0] = 0
+    return totalRoi
+
+def get_POH_domain(dir,rectangle,POHSingleFiles=False):
+    """
+    Get the probability of hail (POH) over a given region,
+     optionally, get per 5-min timestep the max of the region.
+
+    Args:
+    -----
+    dir: str
+     The directory containing the POH files.
+    rectangle: tuple
+     A tuple specifying the bounds of the region as (xmin, xmax, ymin, ymax).
+    POHSingleFiles: bool, optional
+     If set to true, also the maximum POH of the region at 5-min interval
+     is returned. Default is False
+
+
+    Returns:
+    --------
+    POH_domain: ndarray
+     array containing the POH data for the region
+    maxPOHOverTime: ndarray or None
+     if POHSingleFiles is True, an array containing the max POH over 
+     the region, per 5-min timestep 
+    """
+    POH_Dir = os.path.join(dir,"POH")
+    POH_file = glob.glob(os.path.join(POH_Dir, "*2400VL.845.h5"))
+    POH_daily = Dataset(POH_file[0])
+    POH_full = np.transpose(np.ma.getdata(POH_daily["dataset1"]["data1"]["data"][:]))
+    POH_domain = POH_full[
+        coordy2ind(rectangle[3]) : coordy2ind(rectangle[2]),
+        coordx2ind(rectangle[0]) : coordx2ind(rectangle[1]),
+    ]
+    maxPOHOverTime=None
+    if np.count_nonzero(~np.isnan(POH_domain)) != 0 and POHSingleFiles:
+        maxPOHOverTime = get_single_POH(POH_Dir,rectangle)
+    return POH_domain, maxPOHOverTime
+
+def get_single_POH(dir,rectangle):
+    """
+    Get per 5-min timestep the max POH of the region.
+
+    Args:
+    -----
+    dir: str
+     The directory containing the POH files.
+    rectangle: tuple
+     A tuple specifying the bounds of the region as (xmin, xmax, ymin, ymax).
+
+    Returns:
+    --------
+    maxPOH: ndarray
+     An array containing the max POH over the region, per 5-min timestep 
+    """    
+    all_POH_files = glob.glob(os.path.join(dir, "SingleFiles", "*.h5"))
+    all_POH_files = np.sort(all_POH_files)
+    maxPOH = np.zeros(len(all_POH_files))
+    for i, file in enumerate(all_POH_files):
+        POH_single = (Dataset(file))["dataset1"]["data1"]["data"][:]
+        POH_single_domain = POH_single[
+            coordy2ind(rectangle[3]) : coordy2ind(rectangle[2]),
+            coordx2ind(rectangle[0]) : coordx2ind(rectangle[1]),
+    ]
+        maxPOH[i] = np.nanmax(POH_single_domain)
+
+    return maxPOH
+
+
+def get_totalSumRois(totalRoi,prd):
+    """
+    Get the rainfall depth over the region of interest
+
+    Args:
+    -----
+    totalRoi: ndarray
+     An array containing precipitation (mm/h) within the specified region per timestep.
+    prd: str
+     The product for which the data is intended, either "CPC" or "RZC"
+
+    Returns:
+    --------
+    totalSumRoiS: ndarray
+     containing rainfall depth over given time period
+
+    """    
+    if prd == "AQC":
+        totalSumRoi = np.sum(totalRoi, axis=2)
+    else:
+        totalSumRoi = np.sum(totalRoi / 12.0, axis=2)
+        
+    totalSumRoiS = np.around(totalSumRoi, decimals=1)
+    totalSumRoiS[totalSumRoiS < 0] = np.NaN
+
+    return totalSumRoiS
---
+++ utils/transformation.py
@@ -0,0 +1,128 @@
+"""
+Methods for transforming data
+"""
+from datetime import datetime, timedelta
+import os
+import math
+import numpy as np
+from numba import jit
+
+@jit
+def coordx2ind(coordx):
+    x = (coordx - 2255000) / 1000
+    return int(x)
+
+
+@jit
+def coordy2ind(coordy):
+    y = (1480000 - coordy) / 1000
+    return int(y)
+
+
+# Result in radians, angle between the radar and the center of the ROI
+@jit
+def angle(radar_location_0, radar_location_1, poi):
+    return np.arctan2((radar_location_0 - poi[0]), -1 * (radar_location_1 - poi[1]))
+
+
+# Result is returned in meters
+@jit
+def beamHeight(R, elevation, radarHeight=0, earthRadius=6347):
+    bh = (
+        np.sqrt(
+            R**2
+            + (4 / 3 * earthRadius) ** 2
+            + (2 * R * 4 / 3 * earthRadius * math.sin(np.deg2rad(elevation)))
+        )
+        - (4 / 3 * earthRadius)
+        + radarHeight / 1000
+    )
+    return bh * 1000
+
+
+def fname2timestring(fname, newline=True):
+    bname = fname.split(os.path.sep)[-1].split(os.path.sep)[-1]
+    year = '20'+bname[3:5]
+    day = bname[5:8]
+    hour = bname[8:10]
+    minute = bname[10:12]
+    time = datetime.strptime(year + ' ' + day, '%Y %j')
+    if newline:
+        dtstring = time.strftime('%d-%m-%Y')+"\n"+hour+":"+minute+"UTC"
+    else:
+        dtstring = time.strftime('%d-%m-%Y')+" "+hour+":"+minute+"UTC"
+    return (dtstring)
+
+
+def get_file_str(name):
+    """
+    Get a dict with information such as date, time product from the filename
+    
+    name: str
+     file name with structure prdYYDOYHHMM*
+
+    Returns:
+    file_info: dict
+     containing information about date, time, product and name of specific file
+    """
+    file_info = {
+        "bname": name,
+        "prd": name[:3],
+        "year": "20" + name[3:5],
+        "day": name[5:8],
+        "hour": name[8:10],
+        "minute": name[10:12],
+        "time": datetime.strptime("20" + name[3:5] + " " + name[5:8], "%Y %j"),
+        "date": datetime.strptime("20" + name[3:5] + name[5:8] + name[8:10] + name[10:12], "%Y%j%H%M"),
+        "eventCodeName": name[0:8],
+    }
+    return file_info
+
+
+
+def nearest_5min(df):
+    """
+    Rounds time to the closest 5 min
+
+    Args:
+    -----
+    df: DataFrame
+     Dataframe containing time column
+    
+    Returns:
+    df: DataFrame
+     Dataframe with min rounded to 5 min.
+    """
+    for i in range(0, len(df["time"])):
+        # Calculate the number of seconds until the nearest 5 minute interval
+        t = datetime.strptime(df["time"][i], "%Y-%m-%d %H:%M:%S")
+        seconds = (t.minute % 5) * 60 + t.second
+        # Subtract that number of seconds from the current time to get the nearest 5 minute interval
+        nearest = t - timedelta(seconds=seconds)
+        # If the nearest interval is more than 2.5 minutes away, round up to the next interval instead
+        if seconds > 150:
+            nearest += timedelta(minutes=5)
+        df["time"][i] = nearest
+    return df
+
+
+def make_timeserie(bname,fname):
+    """
+    Makes a timeserie of the event with a 5-min interval
+
+    Args:
+    -----
+    bname: dict
+     containing start date and time of the event
+    fname: dict
+     containing end time of the event
+    
+    Returns:
+    --------
+    timeserie: ndarray
+     containing timeseries with 5-min interval
+    """
+    
+    n_incr = int((fname['date'] - bname['time']).total_seconds() / (60 * 5))
+    timeserie = bname['time'] + np.array([timedelta(minutes=5*i)  for i in range(n_incr + 1)])
+    return timeserie
