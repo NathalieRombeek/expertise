6735581c8747b7d28daa160a300b4b6519287b68
Nathalie Rombeek <nathalie.rombeek@meteoswiss.ch>
2023-01-11 13:30:40 +0100

    example running expertise

---
+++ example.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from expertise import run_expertise
+from utils.transformation import make_rg
+
+rg1 = make_rg("BLZ", 30, 2720913, 1116587)
+rg2 = make_rg("BLZ", 42, 2720913, 1116587)
+rain_gauges = [rg1, rg2]
+
+date=datetime(2022,6,10)
+product="RZC"
+
+run_expertise(
+    date,
+    product,
+    file_dir="/scratch/nrombeek/expertise_package/example/",
+    rg_args = rain_gauges,
+    opt_kwargs={"visibMap": True})
