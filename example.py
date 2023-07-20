979995725194cec66160ec0ee078cff0f113b0d5
NathalieRombeek <102802674+NathalieRombeek@users.noreply.github.com>
2023-07-20 08:22:35 +0200

    change optional arguments

--- example.py
+++ example.py
@@ -10,26 +10,31 @@ rain_gauges = [rg1, rg2]
 start_date = datetime(2022, 7, 20, 16, 0)
 end_date = datetime(2022, 7, 21, 14, 0)
 
-product = "RZC"
-
-run_expertise(
-    start_date,
-    end_date,
-    product,
-    file_dir="../example", #path where output needs to be stored
-    archive="/store/msrad/radar/swiss/", #path where input is stored (or use local path containg .zip and optionally .prd file)
-    name="Carrerabach",
-    regionRectangle=[2739000, 2746000, 1178000, 1185000],
-    rg_args=rain_gauges,
-    opt_kwargs={
-        "visibMap": True,
+opt_kwargs = {
+        "visibMap": False,
         "POHfiles": True,
-        "POHSingleFiles": True,
+        "POHSingleFiles": False,
         "singleFiles": False,
         "roiMap": False,
         "visibMap": False,
         "useOsm": False,
         "UseOsmSingleFiles": False,
         "make_kml_file": False,
-    },
+    }
+
+product = "RZC"
+name = "Carrerabach"
+regionRectangle = [2739000, 2746000, 1178000, 1185000]
+archive="/store/msrad/radar/swiss/"
+
+run_expertise(
+    start_date,
+    end_date,
+    product,
+    "../example/", #path where output needs to be stored
+    archive,#"/store/msrad/radar/swiss/", #path where input is stored (or use local path containig .zip and optionally .prd file)
+    name,
+    regionRectangle,
+    *rg_args,
+    **opt_kwargs
 )
